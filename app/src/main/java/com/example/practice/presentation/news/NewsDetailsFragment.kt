d59b4eb8d721d04ef97d2b6c37f1c4c77217df61
e.zaharova <zakh14@bk.ru>
2021-12-16 19:55:44 +0300

    task 4: news details screen added(only layout)

---
+++ app/src/main/java/com/example/practice/presentation/news/NewsDetailsFragment.kt
@@ -0,0 +1,6 @@
+package com.example.practice.presentation.news
+
+import androidx.fragment.app.Fragment
+import com.example.practice.R
+
+class NewsDetailsFragment : Fragment(R.layout.fragment_news_details)
